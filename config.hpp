6434d8030fff298224f0c47a81b5db52fadbe9b0
Ayrton Sparling <ayrton@sparling.us>
2017-01-21 12:50:44 -0800

    Initial

---
+++ config.hpp
@@ -0,0 +1,3 @@
+// the configured options and settings for PROJECT
+#define PROJECT_VERSION_MAJOR 1
+#define PROJECT_VERSION_MINOR 0
---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "config.hpp"
+
+int main() {
+
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
